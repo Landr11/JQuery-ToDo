a18dd4d85ce08bfb32f328aa02524c935da12c99
Landr11 <citywokb3@yahoo.com>
2019-07-05 12:02:08 +0100

    add JQuery logic and CSS Styling

---
+++ assets/js/todos.js
@@ -0,0 +1,36 @@
+//Check off Specific Todos by clicking
+
+$("ul").on("click", "li", function() {
+  $(this).toggleClass("completed");
+});
+
+// Click on X to delete Todo
+
+$("ul").on("click", "span", function(event) {
+  $(this)
+    .parent()
+    .fadeOut(function() {
+      $(this).remove;
+    });
+
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    //Grabbing New ToDo from input
+    var todoText = $(this).val();
+    //set input empty
+    $(this).val("");
+
+    //Create new Todo with the Grabbed Text
+
+    $("ul").append(
+      "<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "<lil>"
+    );
+  }
+});
+
+$(".toggle-add").click(function() {
+  $("input[type='text']").fadeToggle();
+});
